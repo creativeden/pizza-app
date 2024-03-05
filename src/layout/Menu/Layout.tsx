616d4ce42ed740e9da2662aa66cc82c74c8bd481
creativeden <eurodeny@gmail.com>
2024-03-05 22:29:30 +0700

    Lesson 87

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -1,12 +1,30 @@
 import { Link, Outlet } from 'react-router-dom';
 import styles from './Layout.module.css';
+import Button from '../../components/Button/Button';
 
 export function Layout() {
 	return <div className={styles['layout']}>
-		<nav className={styles['menu']}>
-			<Link to="/">Menu</Link>
-			<Link to="/cart">Cart</Link>
-		</nav>
+		<div className={styles['sidebar']}>
+			<div className={styles['user']}>
+				<img width={90} height={90} src="/avatar.png" alt="Avatar" />
+				<div className={styles['name']}>User Pizza</div>
+				<div className={styles['email']}>upizza@test.loc</div>
+			</div>
+			<nav className={styles['menu']}>
+				<Link to="/" className={styles['link']}>
+					<img src="/menu-icon.svg" alt="ico menu" />
+					Menu
+				</Link>
+				<Link to="/cart" className={styles['link']}>
+					<img src="/cart-icon.svg" alt="ico cart" />
+					Cart
+				</Link>
+			</nav>
+			<Button className={styles['exit']}>
+				<img src="/exit-icon.svg" alt="ico exit" />
+				Exit
+			</Button>
+		</div>
 		<div>
 			<Outlet />
 		</div>
