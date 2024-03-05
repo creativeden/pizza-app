a00ca2d0e14e5d9a975df3486feeccf0b27940d8
creativeden <eurodeny@gmail.com>
2024-03-05 22:50:17 +0700

    Lesson 89

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -1,6 +1,7 @@
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import styles from './Layout.module.css';
 import Button from '../../components/Button/Button';
+import cn from 'classnames';
 
 export function Layout() {
 	return <div className={styles['layout']}>
@@ -11,14 +12,18 @@ export function Layout() {
 				<div className={styles['email']}>upizza@test.loc</div>
 			</div>
 			<nav className={styles['menu']}>
-				<Link to="/" className={styles['link']}>
+				<NavLink to="/" className={({ isActive }) => cn(styles['link'], {
+					[styles.active]: isActive
+				})}>
 					<img src="/menu-icon.svg" alt="ico menu" />
 					Menu
-				</Link>
-				<Link to="/cart" className={styles['link']}>
+				</NavLink>
+				<NavLink to="/cart" className={({ isActive }) => cn(styles['link'], {
+					[styles.active]: isActive
+				})}>
 					<img src="/cart-icon.svg" alt="ico cart" />
 					Cart
-				</Link>
+				</NavLink>
 			</nav>
 			<Button className={styles['exit']}>
 				<img src="/exit-icon.svg" alt="ico exit" />
