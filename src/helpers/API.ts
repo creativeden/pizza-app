9bc0b3b081d74b09c29f39d3daf68eed449aff81
creativeden <eurodeny@gmail.com>
2024-03-06 19:33:24 +0700

    Lesson 94

---
+++ src/helpers/API.ts
@@ -0,0 +1,1 @@
+export const PREFIX = 'https://c5cb00968e032a16.mokky.dev';
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,23 +1,48 @@
+import { useEffect, useState } from 'react';
 import Headling from '../../components/Headling/Headling';
 import ProductCard from '../../components/ProductCard/ProductCard';
 import Search from '../../components/Search/Search';
+import { Product } from '../../interfaces/product.interface';
 import styles from './Menu.module.css';
 
 export function Menu() {
+	const [products, setProducts] = useState<Product[]>([]);
+
+	const getMenu = async () => {
+		try {
+			const res = await fetch('https://6396dca824fa79e2.mokky.dev/products');
+			if (!res.ok) {
+				return;
+			}
+			const data = await res.json() as Product[];
+			setProducts(data);
+		} catch (e) {
+			console.error(e);
+			return;
+		}
+	};
+
+	useEffect(() => {
+		getMenu();
+	}, []);
+
 	return <>
 		<div className={styles['head']}>
 			<Headling>Menu</Headling>
 			<Search placeholder='Write what you look' />
 		</div>
 		<div>
-			<ProductCard 
-				id={1} 
-				title='Delishes' 
-				description='Salami, Rukola, Tomato, Olive' 
-				rating={4.5} 
-				price={300} 
-				image='https://dodopizza-a.akamaihd.net/static/Img/Products/f8bcc0d18f5a4817a720a159f0f8c37c_584x584.webp' 
-			/>
+			{products.map(p => (
+				<ProductCard 
+					key={p.id}
+					id={p.id} 
+					title={p.name} 
+					description={p.ingredients.join(', ')} 
+					rating={p.rating} 
+					price={p.price} 
+					image={p.image} 
+				/>
+			))}
 		</div>
 	</>;
 }
