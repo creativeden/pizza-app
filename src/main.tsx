3d5d40ebeb609974c3789b6b8369fb8165188df1
creativeden <eurodeny@gmail.com>
2024-04-06 23:20:35 +0700

    Lesson 110

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,8 @@ import { AuthLayout } from './layout/Auth/AuthLayout';
 import { Login } from './pages/Login/Login';
 import { Register } from './pages/Register/Register';
 import { RequireAuth } from './helpers/RequreAuth';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const Menu = lazy(() => import('./pages/Menu/Menu'));
 
@@ -75,6 +77,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
 	</React.StrictMode>
 );
